9f01442e1321ed25d18bdb9068cb3c7ff1a23fb7
David Caetano <davidcoimbra73@hotmail.com>
2024-07-28 20:10:57 +0100

    Now a single script starts INT collector and visualizer

---
+++ INT/start_INT_sripts.py
@@ -0,0 +1,12 @@
+import subprocess
+# Define the scripts to be launched
+scripts = ['INT/receive/collector_influxdb.py', 'INT/visualizer/visualizer.py']
+
+def main():
+    # Launch the scripts in new terminal tabs
+    for script in scripts:
+        print(f"Starting {script} in a new terminal tab...")
+        subprocess.Popen(['gnome-terminal', '--tab', '--', 'bash', '-c', f"python3 {script}; exec bash"])
+
+if __name__ == "__main__":
+    main()
