bf66b8edc60fcb9d0ec07bab845dedbfac95d406
David Caetano <davidcoimbra73@hotmail.com>
2024-07-30 00:50:35 +0100

    chore: Update start_INT_sripts.py to use "Window" instead of "tab" for launching scripts

--- INT/start_INT_sripts.py
+++ INT/start_INT_sripts.py
@@ -5,7 +5,7 @@ scripts = ['INT/receive/collector_influxdb.py', 'INT/visualizer/visualizer.py']
 def main():
     # Launch the scripts in new terminal tabs
     for script in scripts:
-        print(f"Starting {script} in a new terminal tab...")
+        print(f"Starting {script} in a new terminal Window...")
         subprocess.Popen(['gnome-terminal', '--tab', '--', 'bash', '-c', f"python3 {script}; exec bash"])
 
 if __name__ == "__main__":
