550449f317db0753aaa2d062f56496d9317fde0d
David Caetano <davidcoimbra73@hotmail.com>
2024-08-01 18:05:42 +0100

    flows keep colours and better names

--- INT/visualizer/visualizer.py
+++ INT/visualizer/visualizer.py
@@ -21,85 +21,115 @@ else:
 # Lista de cores para serem usadas
 colors = ['red', 'green', 'blue', 'orange', 'purple', 'brown', 'pink', 'gray', 'cyan', 'magenta']
 
-# Dicionário para armazenar cores para cada caminho
+# Dicionário para armazenar cores para cada fluxo
 path_colors = {}
 
-# Função para atribuir uma cor a um caminho
-def assign_color(path):
-    if path not in path_colors:
+# Dicionário para armazenar índices das arestas para cada fluxo
+edge_flow_indices = {}
+
+# Função para atribuir uma cor a um fluxo
+def assign_color(flow_identifier):
+    if flow_identifier not in path_colors:
         if len(colors) > 0:
-            path_colors[path] = colors.pop(0)
+            path_colors[flow_identifier] = colors.pop(0)
         else:
             # Se as cores predefinidas acabarem, gerar uma cor aleatória
-            path_colors[path] = "#{:06x}".format(random.randint(0, 0xFFFFFF))
-    return path_colors[path]
+            path_colors[flow_identifier] = "#{:06x}".format(random.randint(0, 0xFFFFFF))
+    return path_colors[flow_identifier]
 
 # Função para atualizar o gráfico
+# Função para atualizar o gráfico
 def update_graph(G, new_data, edge_update_time):
-    unique_paths = set()
+    unique_flows = set()
     
     for point in new_data:
-        path = point['path']
         flow_label = point['flow_label']
         src_ip = point['src_ip']
         dst_ip = point['dst_ip']
         
-        path_identifier = f"{path}_{flow_label}_{src_ip}_{dst_ip}"  # Create a unique identifier for each path
-        unique_paths.add(path_identifier)
+        flow_identifier = f"{flow_label}_{src_ip}_{dst_ip}"  # Create a unique identifier for each flow
+        unique_flows.add(flow_identifier)
     
-    for path_identifier in unique_paths:
-        path, flow_label, src_ip, dst_ip = path_identifier.split('_')
+    for flow_identifier in unique_flows:
+        flow_label, src_ip, dst_ip = flow_identifier.split('_')
         
-        path_color = assign_color(path_identifier)  # Atribui uma cor ao caminho
-        print("\nPath:", path, "Flow Label:", flow_label, "Source IP:", src_ip, "Destination IP:", dst_ip, "Color:", path_color)
-        switches = path.split('-')
+        flow_color = assign_color(flow_identifier)  # Assign a color to the flow
+        print("\nFlow Label:", flow_label, "Source IP:", src_ip, "Destination IP:", dst_ip, "Color:", flow_color)
         
-        for i in range(len(switches) - 1):
-            egress_switch_id = int(switches[i])
-            ingress_switch_id = int(switches[i + 1])
-            edge = (egress_switch_id, ingress_switch_id, path_color)  # Include path color in the edge
-            if edge not in edge_colors:
-                edge_colors[edge] = path_color
-            print('Edge:', edge)
-            edge_update_time[edge] = datetime.now()  # Atualiza o tempo da última atualização
-    
-       
+        # Find paths for the specific flow
+        paths = [point['path'] for point in new_data if point['flow_label'] == flow_label and point['src_ip'] == src_ip and point['dst_ip'] == dst_ip]
+        
+        # Reset the edge indices for this flow
+        edge_flow_indices[flow_identifier] = {}
+        
+        for path in paths:
+            switches = path.split('-')
+            
+            for i in range(len(switches) - 1):
+                egress_switch_id = int(switches[i])
+                ingress_switch_id = int(switches[i + 1])
+                edge = (egress_switch_id, ingress_switch_id, flow_identifier)  # Include flow identifier in the edge
+                
+                if edge not in edge_colors:
+                    edge_colors[edge] = flow_color
+                if flow_identifier not in edge_flow_indices:
+                    edge_flow_indices[flow_identifier] = {}
+                if edge not in edge_flow_indices[flow_identifier]:
+                    edge_flow_indices[flow_identifier][edge] = len(edge_flow_indices[flow_identifier]) + 1  # Assign a unique index per flow
+                # print('Edge:', edge, 'Index:', edge_flow_indices[flow_identifier][edge])
+                edge_update_time[edge] = datetime.now()  # Update the last update time
 
 # Função para visualizar o gráfico
-def visualize_graph(G, edge_colors):
-    plt.clf()  # Limpar a figura atual
+def visualize_graph(G, edge_colors, edge_flow_indices):
+    plt.clf()  # Clear the current figure
 
-    # Desenhar todos os nós
+    # Draw all nodes
     nx.draw_networkx_nodes(G, pos, node_size=400, edgecolors="black")
 
-    # Desenhar todas as arestas em cinza claro
+    # Draw all edges in light gray
     nx.draw_networkx_edges(G, pos, edgelist=G.edges(), edge_color="lightgray", style="dashed", connectionstyle="arc3,rad=0.0")
 
-    # Desenhar as arestas com cores atribuídas
+    # Draw edges with assigned colors
+    edge_labels = {}
     for i, (edge, color) in enumerate(edge_colors.items()):
-        egress_switch_id, ingress_switch_id, _ = edge
+        egress_switch_id, ingress_switch_id, flow_identifier = edge
+        rad = (i % 10) * 0.1 - 0.2  # Adjust arc radius for each edge
+        
         nx.draw_networkx_edges(
             G, pos, edgelist=[(egress_switch_id, ingress_switch_id)], 
             edge_color=color, arrows=True, 
-            connectionstyle=f"arc3,rad={(i % 10) * 0.1 - 0.2}"  # Adjust arc radius for each edge
+            connectionstyle=f"arc3,rad={rad}"
         )
+        
+        if flow_identifier in edge_flow_indices and edge in edge_flow_indices[flow_identifier]:
+            flow_index = edge_flow_indices[flow_identifier][edge]  # Get the flow-specific index for the edge
+            edge_labels[(egress_switch_id, ingress_switch_id, rad)] = (flow_index, color)  # Store index and color
 
-    # Desenhar rótulos dos nós
+    # Add labels to nodes
     nx.draw_networkx_labels(G, pos, font_size=13, font_family="sans-serif")
 
-    # Criar uma legenda para os fluxos
+    # Add labels to edges
+    for (egress_switch_id, ingress_switch_id, rad), (label, flow_color) in edge_labels.items():
+        # Adjust label position closer to the start of the edge
+        label_pos = (pos[egress_switch_id][0] * 0.8 + pos[ingress_switch_id][0] * 0.2,
+                     pos[egress_switch_id][1] * 0.8 + pos[ingress_switch_id][1] * 0.2 + rad * 0.5)
+        plt.text(label_pos[0], label_pos[1], label, fontsize=10, color=flow_color, ha='center', va='center')
+
+    # Create a legend for flows
     legend_elements = []
-    for path_identifier, color in path_colors.items():
-        path, flow_label, src_ip, dst_ip = path_identifier.split('_')
+    for flow_identifier, color in path_colors.items():
+        flow_label, src_ip, dst_ip = flow_identifier.split('_')
         legend_label = f"Flow {flow_label} ({src_ip} -> {dst_ip})"
         legend_elements.append(Patch(facecolor=color, edgecolor='black', label=legend_label))
 
-    # Adicionar a legenda ao gráfico
-    plt.legend(handles=legend_elements, loc='upper left', fontsize='small')
+    # Adjust figure layout to accommodate the legend on the left
+    plt.subplots_adjust(left=0.23, right=0.98)
+    # Add the legend to the plot
+    plt.legend(handles=legend_elements, loc='center left', bbox_to_anchor=(-0.3, 1.05), fontsize='small')
 
-    # Definir título do gráfico
-    plt.title("Graph with Real-Time Updates")
-    plt.pause(0.7)  # Pausar por 0.7 segundos
+    # Set the title of the plot
+    plt.title("Network Topology Visualizer in Real-Time")
+    plt.pause(0.5)  # Pause for 0.5 seconds
 
 # Inicialização do gráfico
 G = nx.MultiDiGraph()
@@ -122,28 +152,28 @@ pos = {
     5: (0, 1), 11: (2, 1), 12: (4, 1), 6: (6, 1),
     8: (1, 0), 7: (5, 0)}
 
-# Criar figura para o gráfico
+# Criar figura para o gráfico sem trazer para frente
 plt.figure(figsize=(12, 8))
+plt.ion()  # Turn on interactive mode
 
 # Simulação de leitura contínua de dados
 while True:
-    query = 'SELECT path, flow_label, src_ip, dst_ip from "flow_stats" WHERE time > now() - 15s'
+    query = 'SELECT path, flow_label, src_ip, dst_ip from "flow_stats" WHERE time > now() - 1s'
     result = influx_client.query(query)
     points = result.get_points()
     new_data = list(points)
-    # Converter os pontos para uma lista
 
     if new_data:
         update_graph(G, new_data, edge_update_time)
     
-    
     current_time = datetime.now()
     for edge in list(edge_update_time.keys()):
-         if (current_time - edge_update_time[edge]) > timedelta(seconds=2):
-             edge_colors.pop(edge, None)  # Remove the edge color if not updated in the last 2 seconds
+        if (current_time - edge_update_time[edge]) > timedelta(seconds=0.1):
+            edge_colors.pop(edge, None)  # Remove the edge color if not updated in the last 2 seconds
 
-    visualize_graph(G, edge_colors)
+    visualize_graph(G, edge_colors, edge_flow_indices)
     print('\n-= Graph updated at:', datetime.now(), '=-')
-    
+
 # Garantir que todos os gráficos sejam exibidos no final
+plt.ioff()
 plt.show()
