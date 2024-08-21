4dea4a22c70949ae7d3b222bb90f8eed793f6d27
David Caetano <68875095+davidcc73@users.noreply.github.com>
2024-08-21 17:54:51 +0100

    more generic

--- INT/start_INT_sripts.py
+++ INT/start_INT_sripts.py
@@ -1,12 +1,20 @@
 import subprocess
+import os
+
 # Define the scripts to be launched
 scripts = ['INT/receive/collector_influxdb.py', 'INT/visualizer/visualizer.py']
 
 def main():
+    # Verify if x-terminal-emulator is available
+    if not shutil.which("x-terminal-emulator"):
+        print("x-terminal-emulator is not available. Please install a terminal emulator or update the script to use your preferred terminal.")
+        return
+    
     # Launch the scripts in new terminal tabs
     for script in scripts:
-        print(f"Starting {script} in a new terminal Window...")
-        subprocess.Popen(['gnome-terminal', '--tab', '--', 'bash', '-c', f"python3 {script}; exec bash"])
+        script_path = os.path.abspath(script)
+        print(f"Starting {script} in a new terminal window...")
+        subprocess.Popen(['x-terminal-emulator', '-e', f"bash -c 'python3 {script_path}; exec bash'"])
 
 if __name__ == "__main__":
     main()
